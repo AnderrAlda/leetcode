cf20523a8a646cba2853008ba3b7d8e4cc040892
AnderrAlda <anderaldabaldetreku@gmail.com>
2026-08-18 18:18:55 +0200

    learned about hashmap. Almost used 3 for loops, but realised it was dumb.

---
+++ neetcode150/group_anagrams.py
@@ -0,0 +1,15 @@
+    class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = {} #HashMap
+
+        for word in strs:
+            key = "".join(sorted(word))
+
+            if key not in dict:
+                dict[key] = []
+
+            dict[key].append(word)
+
+        return list(dict.values())
+
+
